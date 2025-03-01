02057a18e8d7c0a14150ecec4811fdd319d1f70e
arinakh2005 <arinakholodnytska3@gmail.com>
2025-03-01 20:48:29 +0200

    Feat: Environmental facility CRUD

--- src/modules/environmental-facilities/environmental-facilities.controller.ts
+++ src/modules/environmental-facilities/environmental-facilities.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { EnvironmentalFacilitiesService } from './environmental-facilities.service';
+import { EnvironmentalFacility } from './entities/environmental-facility.entity';
+import { EnvironmentalSubsystem } from '../../enums/environmental-subsystem';
 
 @Controller('environmental-facilities')
 export class EnvironmentalFacilitiesController {
-    constructor(private readonly environmentalFacilitiesService: EnvironmentalFacilitiesService) { }
+    constructor(private readonly facilitiesService: EnvironmentalFacilitiesService) { }
 
     @Get()
-    public findAll() {
-        return this.environmentalFacilitiesService.getAllEnvironmentalFacilities();
+    public async getAll(@Query('subsystemTypes') subsystemTypes?: string): Promise<EnvironmentalFacility[]> {
+        let parsedSubsystemTypes: EnvironmentalSubsystem[] = [];
+
+        if (subsystemTypes) {
+            parsedSubsystemTypes = subsystemTypes.split(',') as EnvironmentalSubsystem[];
+        }
+
+        return this.facilitiesService.findAll(parsedSubsystemTypes);
+    }
+
+    @Get(':id')
+    public async getOne(@Param('id') id: number): Promise<EnvironmentalFacility | null> {
+        return this.facilitiesService.findOne(id);
+    }
+
+    @Post()
+    public async create(@Body() facility: Partial<EnvironmentalFacility>): Promise<EnvironmentalFacility> {
+        return this.facilitiesService.create(facility);
+    }
+
+    @Put(':id')
+    public async update(@Param('id') id: number, @Body() facility: Partial<EnvironmentalFacility>): Promise<EnvironmentalFacility | null> {
+        return this.facilitiesService.update(id, facility);
+    }
+
+    @Delete(':id')
+    public async delete(@Param('id') id: number): Promise<void> {
+        return this.facilitiesService.delete(id);
     }
 }
--- src/modules/environmental-facilities/environmental-facilities.service.ts
+++ src/modules/environmental-facilities/environmental-facilities.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { EnvironmentalFacility } from './entities/environmental-facility.entity';
+import { EnvironmentalSubsystem } from '../../enums/environmental-subsystem';
 
 @Injectable()
 export class EnvironmentalFacilitiesService {
@@ -10,7 +11,32 @@ export class EnvironmentalFacilitiesService {
         private readonly facilityRepository: Repository<EnvironmentalFacility>,
     ) { }
 
-    public async getAllEnvironmentalFacilities(): Promise<EnvironmentalFacility[]> {
-        return await this.facilityRepository.find({ relations: ['indicators'] });
+    public async findAll(subsystemTypes?: EnvironmentalSubsystem[]): Promise<EnvironmentalFacility[]> {
+        const query = this.facilityRepository.createQueryBuilder('facility')
+          .leftJoinAndSelect('facility.indicators', 'indicators');
+
+        if (subsystemTypes && subsystemTypes.length > 0) {
+            query.where('facility.subsystemType IN (:...subsystemTypes)', { subsystemTypes });
+        }
+
+        return query.getMany();
+    }
+
+    public async findOne(id: number): Promise<EnvironmentalFacility | null> {
+        return this.facilityRepository.findOne({ where: { id }, relations: ['indicators'] });
+    }
+
+    public async create(facility: Partial<EnvironmentalFacility>): Promise<EnvironmentalFacility> {
+        return this.facilityRepository.save(this.facilityRepository.create(facility));
+    }
+
+    public async update(id: number, facility: Partial<EnvironmentalFacility>): Promise<EnvironmentalFacility | null> {
+        await this.facilityRepository.update(id, facility);
+
+        return this.findOne(id);
+    }
+
+    public async delete(id: number): Promise<void> {
+        await this.facilityRepository.delete(id);
     }
 }
